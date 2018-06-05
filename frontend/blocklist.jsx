8ed65ca905f5f30582b3efa0eb65db1dfb86f5e0
Robert O'Connor <robertgoconnor2@gmail.com>
2018-06-05 12:32:40 -0400

    Create App, root, and store files

--- frontend/blocklist.jsx
+++ frontend/blocklist.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import App from './components/App';
+
 document.addEventListener('DOMContentLoaded', () => {
   const root = document.getElementById('root');
   ReactDOM.render(<h1>Welcome to Blocklist</h1>, root);
---
+++ frontend/components/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Provider } from 'react-redux';
+import {
+  Route,
+  Redirect,
+  Switch,
+  Link,
+  HashRouter
+} from 'react-router-dom';
+
+const App = () => (
+  <div className="app-container">
+
+    
+
+  </div>
+);
+
+export default App;
